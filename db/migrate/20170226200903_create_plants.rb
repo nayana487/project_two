ca7aa97eb49f1de7ffbe75ad092bdd6e06d6d5ac
Chris Wightman <wightman_christopher@bah.com>
2017-02-27 10:59:24 -0500

    Fixed migration fields to be all strings

--- db/migrate/20170226200903_create_plants.rb
+++ db/migrate/20170226200903_create_plants.rb
@@ -2,12 +2,12 @@ class CreatePlants < ActiveRecord::Migration[5.0]
   def change
     create_table :plants do |t|
       t.string :name
-      t.integer :height
-      t.integer :width
+      t.string :height
+      t.string :width
       t.string :hardiness
-      t.integer :minsoiltemp
-      t.integer :depth
-      t.integer :separation
+      t.string :minsoiltemp
+      t.string :depth
+      t.string :separation
       t.string :watering
       t.string :fertilizing
       t.string :maturity
