2d86502671f52c1dc386f8df449b7fa6b52ce024
Chris Wightman <wightman_christopher@bah.com>
2017-03-01 20:34:28 -0500

    Fixed update function

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,12 @@ class PlantsController < ApplicationController
       @plant = Plant.find(params[:id])
   end
 
+  def update
+    @plant = Plant.find(params[:id])
+    @plant.update(plant_params)
+    redirect_to plant_path(@plant)
+  end
+
   def plant_params
     params.require(:plant).permit(:name, :season_id, :height, :width, :hardiness, :minsoiltemp, :depth, :separation, :watering, :fertilizing, :maturity, :production, :harvest, :img_url)
   end
@@ -28,13 +34,6 @@ class PlantsController < ApplicationController
 #    @plant.season = Season.find_by(name: params[:plant][:season])
     @plant.save
     redirect_to plants_path
-  end
-
-
-  def update
-    @plant = Plant.find(params[:id])
-    @plant = Plant.update(plant_params)
-    redirect_to plant_path(@plant)
   end
 
   def destroy
